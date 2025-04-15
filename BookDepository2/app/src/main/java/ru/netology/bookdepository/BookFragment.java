92fc20d245c6f11b055ae2fba19186099090a106
Your Name <you@example.com>
2025-04-15 12:10:26 +0300

    Тема 8, Работа со списочными элементами интерфейса

--- BookDepository2/app/src/main/java/ru/netology/bookdepository/BookFragment.java
+++ BookDepository2/app/src/main/java/ru/netology/bookdepository/BookFragment.java
@@ -7,11 +7,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.CompoundButton;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class BookFragment extends Fragment {
     private Book mBook;
     private EditText mTitleEditText;
+    private Button mDateButton;
+    private CheckBox mReadedCheckBox;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +37,21 @@ public class BookFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_book, container, false);
 
         mTitleEditText = v.findViewById(R.id.titleEditText);
+        mDateButton = v.findViewById(R.id.book_date);
+        mReadedCheckBox = v.findViewById(R.id.book_readed);
+
         if (mBook != null) {
             mTitleEditText.setText(mBook.getTitle() != null ? mBook.getTitle() : "");
-        }
+            mReadedCheckBox.setChecked(mBook.isReaded());
 
+            // Форматирование даты
+            if (mBook.getDate() != null) {
+                SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+                mDateButton.setText(formatter.format(mBook.getDate()));
+            } else {
+                mDateButton.setText(""); // Или текст по умолчанию
+            }
+        }
         mTitleEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -51,6 +71,11 @@ public class BookFragment extends Fragment {
             }
         });
 
+        mReadedCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (mBook != null) {
+                mBook.setReaded(isChecked);
+            }
+        });
         return v;
     }
 }
